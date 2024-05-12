4cf28db6f30ccdc2774122a045c393c616c98a77
Jim Sober <jimsober@yahoo.com>
2024-05-12 09:20:50 -0600

    removing comments and debug output

--- chucknorris.groovy
+++ chucknorris.groovy
@@ -4,7 +4,6 @@ import groovy.json.*
 //get list of categories
 String categoryUrl = 'https://api.chucknorris.io/jokes/categories'
 jsonTxt = "$categoryUrl".toURL().text
-//println('DEBUG: jsonTxt is ' + jsonTxt)
 
 replacement = {  //remove brackets and quotes from json text
     if (it in ['[', ']', '"']) {
@@ -12,24 +11,19 @@ replacement = {  //remove brackets and quotes from json text
     }
 }
 jsonTxt = jsonTxt.collectReplacements(replacement)
-//println('DEBUG: jsonTxt is ' + jsonTxt)
 def categoryList = jsonTxt.split(',')*.trim() as List
-//println('DEBUG: categoryList is ' + categoryList)
 categoryList.remove('explicit')  //remove explicit category
-//println('DEBUG: categoryList is ' + categoryList)
 
 //loop until user quits
 again = true
 while (again) {
     categoryList.shuffle()
 //    String urlString = 'https://api.chucknorris.io/jokes/random?category={' + categoryList[0] + '}'
-    String urlString = 'https://api.chucknorris.io/jokes/random'
+    String urlString = 'https://api.chucknorris.io/jokes/random'  //workaround
     String urlText = "$urlString".toURL().text
     def json = new JsonSlurper().parseText(urlText)
     System.out.print("\033[H\033[2J")
     System.out.flush()
-//    println json.value.url
-//    println json.value.value
     println json.value
     println()
 
